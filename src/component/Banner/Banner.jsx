f7eace3ee556514f4ab4a1593ef54474c8f1135f
Kader517777 <shakhabdulkader2020@gmail.com>
2023-09-26 21:03:16 +0600

    Responsive for all device

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -12,15 +12,15 @@ const Banner = ({ handleSearch }) => {
     }
 
     return (
-        <div className="lg:container sm:w-full mx-auto bg-[#a25f5ff8]">
-            <div className="hero h-[500px] text-[#a25f5ff8] " style={{ backgroundImage: 'url(https://i.ibb.co/7rc9vh1/fc1f79e18cdc1a12320b9b10ec3e253d.jpg)' }}>
+        <div className="container w-full mx-auto bg-[#a25f5ff8]">
+            <div className="hero h-[400px] md:h-[500px] text-[#a25f5ff8] " style={{ backgroundImage: 'url(https://i.ibb.co/7rc9vh1/fc1f79e18cdc1a12320b9b10ec3e253d.jpg)' }}>
 
                 <div className="hero-overlay bg-[#FFFFFFF3]	"></div>
 
                 <div className="hero-content text-center text-neutral-content flex flex-col ">
                     <h1 className=' text-5xl font-bold text-[#0B0B0B]'>I Grow By Helping People In Need</h1>
                     <div>
-                        <input type="text" onChange={handleChange} className=" h-12 w-96 rounded-l-md text-[#0B0B0B] font-bold text-sm border border-spacing-y-2 border-spacing-l-2" />
+                        <input type="text" onChange={handleChange} className=" h-12 md:w-96 rounded-l-md text-[#0B0B0B] font-bold text-sm border border-spacing-y-2 border-spacing-l-2" />
                         <button onClick={() => handleSearch(`${searchValue}`)} className="btn text-base h-12 bg-[#FF444A] text-[#fff]  rounded-s-none">Search</button>
                     </div>
                 </div>
--- src/component/Catagories/Catagories.jsx
+++ src/component/Catagories/Catagories.jsx
@@ -4,7 +4,7 @@ import Catagory from '../Catagory/Catagory';
 
 const Catagories = ({ catagories }) => {
     return (
-        <div className=' container md:mx-auto grid md:grid-cols-2 lg:grid-cols-4 mx-4  md:my-6  md:gap-10'>
+        <div className=' container md:mx-auto grid md:grid-cols-2 lg:grid-cols-4 px-4 lg:px-0  md:my-6  md:gap-10'>
             {catagories?.map((catagory) => <Catagory key={catagory.id} catagory={catagory}></Catagory>)}
         </div>
     );
